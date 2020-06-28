f36f3865b3c97f2e6714a9648391223e7bc24ebc
Alexandre Amory <amamory@gmail.com>
2020-06-28 12:00:08 -0300

    improving build

--- build.sh
+++ build.sh
@@ -1,23 +1,40 @@
 #export VIVADO=/opt/Xilinx/Vivado/2018.2/bin/vivado
 export VIVADO=/home/lsa/xilinx/2018.2/Vivado/2018.2/bin/vivado
 export VIVADO_DESIGN_NAME=ps_hermes
+export VIVADO_TOP_NAME=$VIVADO_DESIGN_NAME
 
 if [ -f $VIVADO ]; then
   echo "###################################"
   echo "### Creating the Vivado Project ###"
   echo "###################################"
-  $VIVADO -mode batch -source build.tcl
+  $VIVADO -mode batch -source build.tcl -notrace
   echo "#########################"
   echo "######## Synthesis ######"
   echo "#########################"
-  $VIVADO -mode batch -source build_bitstream_export_sdk.tcl
-  echo "#########################"
-  echo "### Loading bitstream ###"
-  echo "#########################"
-  $VIVADO -mode batch -source download_bitstream.tcl
-  echo "#########################"
-  echo "### Bitstream loaded ####"
-  echo "#########################"
+  $VIVADO -mode batch -source build_bitstream_export_sdk.tcl -notrace
+  # count xml files to decide whether this project is a leaf custom IP or a design that uses custom IPs
+  # For instance, considering this command executed in the project root dir:
+  #$ find hw/ips/*/ -name *.xml
+  # This is a typical output of a design that uses 3 custom IPs
+  #hw/ips/hermes-router-axis-ip/hw/ips/hermes_router_axis_ip/component.xml
+  #hw/ips/m-axis-dip-ip/hw/ips/m_axis_dip_ip/component.xml
+  #hw/ips/s-axis-led-ip/hw/ips/s_axis_led_ip/component.xml
+  # However, a leaf custom IP (i.e. and IP that does not use other custom IPs) would have this kind of file tree
+  #hw/ips/axis_s_const/component.xml
+  # In summary, if the component.xml is found 3 dir layers below the root dir, then this project is a leaf custom IP
+  # if the component.xml is found in more than 3 dir layers, then this project uses custom IPs
+  # Finally, there is no component.xml at all under hw/ips, then this project do not uses any custom IP
+  leafIPs=$(find hw/ips/*/ -maxdepth 1 -name *.xml | wc -l) 
+  if [ "$leafIPs" -ge 0 ]; 
+  then
+    echo "#########################"
+    echo "### Loading bitstream ###"
+    echo "#########################"
+    $VIVADO -mode batch -source download_bitstream.tcl -notrace
+    echo "#########################"
+    echo "### Bitstream loaded ####"
+    echo "#########################"
+  fi;
   # check whether there is any software to be compiled, i.e., if there is any dir inside src/
   list_dirs=`ls -d ./src/*/ 2> /dev/null`
   # build a bash list 
--- build.tcl
+++ build.tcl
@@ -165,32 +165,36 @@ if {[string equal [get_filesets -quiet sim_1] ""]} {
 }
 
 # Set 'sim_1' fileset object
-set obj [get_filesets sim_1]
-set_property -name "sim_mode" -value "post-implementation" -objects $obj
-#TODO assuming the testbench name is tb. is it possible to find it out automatically ?
-set_property -name "top" -value "tb" -objects $obj
-set_property -name "top_lib" -value "xil_defaultlib" -objects $obj
 # Import testbenches, waveform files, etc if they exist
+set obj [get_filesets sim_1]
 set sim_files [glob -nocomplain -directory $origin_dir/hw/hdl/sim *{*.vhd,*.v,*.sv}*]
 
-foreach sim_file $sim_files {
-  set file "[file normalize "$sim_file"]"
-  add_files -quiet -fileset sim_1 $file
-  set file_obj [get_files -of_objects [get_filesets sim_1] $file]
-  set extension [string tolower [file extension $file]]
-  if {$extension == ".vhd"} {
-    set hdl_type "VHDL"
-  } elseif {$extension == ".v"} {
-    set hdl_type "Verilog"
-  } elseif {$extension == ".sv"} {
-    set hdl_type "SystemVerilog"
-  } else {
-    puts "ERROR: HDL extension $extension is not supported"
-    return
+# if there is a testbench, then add few more properties 
+if {[llength $sim_files] > 0} {
+  set_property -name "sim_mode" -value "post-implementation" -objects $obj
+  #TODO assuming the testbench name is tb. is it possible to find it out automatically ?
+  set_property -name "top" -value "tb" -objects $obj
+  set_property -name "top_lib" -value "xil_defaultlib" -objects $obj
+
+  foreach sim_file $sim_files {
+    set file "[file normalize "$sim_file"]"
+    add_files -quiet -fileset sim_1 $file
+    set file_obj [get_files -of_objects [get_filesets sim_1] $file]
+    set extension [string tolower [file extension $file]]
+    if {$extension == ".vhd"} {
+      set hdl_type "VHDL"
+    } elseif {$extension == ".v"} {
+      set hdl_type "Verilog"
+    } elseif {$extension == ".sv"} {
+      set hdl_type "SystemVerilog"
+    } else {
+      puts "ERROR: HDL extension $extension is not supported"
+      return
+    }
+    set_property -name "file_type" -value $hdl_type -objects $file_obj
+    #TODO can i replace the lib by work ?
+    set_property -name "library" -value "work" -objects $file_obj
   }
-  set_property -name "file_type" -value $hdl_type -objects $file_obj
-  #TODO can i replace the lib by work ?
-  set_property -name "library" -value "work" -objects $file_obj
 }
 
 # waveform files are simply added to the project. no property is set
@@ -247,7 +251,7 @@ if {[llength $block_files] == 1} {
   # Generate the wrapper
   make_wrapper -files [get_files *${design_name}.bd] -top
   # It is asuming that if there is a block file, its wrapper will be the top 
-  add_files -norecurse ./vivado/${design_name}/${design_name}.srcs/sources_1/bd/${design_name}/hdl/${top_name}.v
+  add_files -norecurse ./vivado/${design_name}/${design_name}.srcs/sources_1/bd/${design_name}/hdl/${design_name}_wrapper.v
 } elseif {[llength $block_files] > 1} {
   puts "ERROR: multiple block files found. The script only supports one block per design"
   return
